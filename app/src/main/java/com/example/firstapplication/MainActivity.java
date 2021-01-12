dadffc92a9cb52ae6476748c43faf53a4cdbf5ae
IshaKapadia <202012046@daiict.ac.in>
2021-01-13 03:07:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/firstapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.firstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+   public void sendNow(View view)
+   {
+       Toast.makeText(this, "Sending Data From App...", Toast.LENGTH_SHORT).show();
+   }
+    public void ReceiveNow(View view)
+    {
+        Toast.makeText(this, "Receiving Data From App...", Toast.LENGTH_SHORT).show();
+    }
+    public void DeleteNow(View view)
+    {
+        Toast.makeText(this, "Deleting Data From App...", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
